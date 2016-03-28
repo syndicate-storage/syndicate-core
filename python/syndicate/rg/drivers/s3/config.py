49758920614f40d2f7a5a17ca9293af4c902d7c8
Zack Williams <zdw@artisancomputer.com>
2016-03-28 16:41:53 -0700

    fix shebang lines, make dependent on python

--- python/syndicate/rg/drivers/s3/config.py
+++ python/syndicate/rg/drivers/s3/config.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 CONFIG = {
    
    "BUCKET":   "sd_s3_testbucket",
-   "EXEC_FMT": "/usr/bin/python -m syndicate.rg.gateway",
+   "EXEC_FMT": "/usr/bin/env python -m syndicate.rg.gateway",
    "DRIVER":   "syndicate.rg.drivers.s3"
 }
