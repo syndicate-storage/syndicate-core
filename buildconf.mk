412121357d16576169922fc08ad3cf81ed4afd27
Zack Williams <zdw@artisancomputer.com>
2016-04-05 18:31:09 -0700

    fix space/tab issue

--- buildconf.mk
+++ buildconf.mk
@@ -27,16 +27,15 @@ BUILD_PROTOBUFS_DIRS    := $(BUILD_PROTOBUFS_CPP) $(BUILD_PROTOBUFS_PYTHON)
 BUILD_MS       := $(BUILD)/ms
 BUILD_MS_TOOLS := $(BUILD_BINDIR)
 BUILD_MS_DIRS  := $(BUILD_MS)/common \
-									$(BUILD_MS)/protobufs \
-									$(BUILD_MS)/storage \
-									$(BUILD_MS)/storage/backends \
-									$(BUILD_MS)/MS \
-									$(BUILD_MS)/MS/methods \
-									$(BUILD_MS)/google \
-									$(BUILD_MS)/google/protobuf \
-									$(BUILD_MS)/google/protobuf/internal \
-									$(BUILD_MS)/google/protobuf/compiler
-
+	$(BUILD_MS)/protobufs \
+	$(BUILD_MS)/storage \
+	$(BUILD_MS)/storage/backends \
+	$(BUILD_MS)/MS \
+	$(BUILD_MS)/MS/methods \
+	$(BUILD_MS)/google \
+	$(BUILD_MS)/google/protobuf \
+	$(BUILD_MS)/google/protobuf/internal \
+	$(BUILD_MS)/google/protobuf/compiler
 INSTALL_MS     := $(BUILD_LIBEXEC_DIR)/ms
 
 # libsyndicate
