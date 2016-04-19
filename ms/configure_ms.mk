aa0e86efae6873cd381dc8fd53708e993bb13faf
Jude Nelson <judecn@gmail.com>
2016-04-19 01:17:25 -0400

    whitespace

--- ms/configure_ms.mk
+++ ms/configure_ms.mk
@@ -9,14 +9,14 @@ BUILD_MS ?=
 # Where to put generated pub/priv keys
 KEY_FILES ?= $(CONFIG_DIR)
 
-MS_APP_NAME					?= syndicate-ms
+MS_APP_NAME			?= syndicate-ms
 MS_APP_PUBLIC_HOST	?= localhost
 MS_APP_ADMIN_EMAIL	?= syndicate-ms@example.com
 
 MS_APP_ADMIN_PUBLIC_KEY		?= $(KEY_FILES)/admin.pub
 MS_APP_ADMIN_PRIVATE_KEY	?= $(KEY_FILES)/admin.pem
 
-MS_APP_PUBLIC_KEY		?= $(KEY_FILES)/syndicate.pub
+MS_APP_PUBLIC_KEY	?= $(KEY_FILES)/syndicate.pub
 MS_APP_PRIVATE_KEY	?= $(KEY_FILES)/syndicate.pem
 
 $(MS_APP_ADMIN_PRIVATE_KEY):
@@ -35,12 +35,12 @@ $(BUILD_MS)/app.yaml: $(CONFIG_DIR)/app.yamlin $(MS_APP_ADMIN_PUBLIC_KEY) $(MS_A
 	mkdir -p "$(@D)"
 	cat "$<" | \
 		sed -e 's~@MS_APP_NAME@~$(MS_APP_NAME)~g;' \
-				-e 's~@MS_APP_PUBLIC_HOST@~$(MS_APP_PUBLIC_HOST)~g;' \
-				-e 's~@MS_APP_ADMIN_EMAIL@~$(MS_APP_ADMIN_EMAIL)~g;' \
-				-e 's~@MS_APP_ADMIN_PUBLIC_KEY@~$(shell cat $(MS_APP_ADMIN_PUBLIC_KEY) | tr "\n" "@  " | sed 's/@/\\n    /g')~g;' \
-				-e 's~@MS_APP_PUBLIC_KEY@~$(shell cat $(MS_APP_PUBLIC_KEY) | tr "\n" "@" | sed 's/@/\\n    /g')~g;' \
-				-e 's~@MS_APP_PRIVATE_KEY@~$(shell cat $(MS_APP_PRIVATE_KEY) | tr "\n" "@" | sed 's/@/\\n    /g')~g;' \
-				> "$@"
+			-e 's~@MS_APP_PUBLIC_HOST@~$(MS_APP_PUBLIC_HOST)~g;' \
+			-e 's~@MS_APP_ADMIN_EMAIL@~$(MS_APP_ADMIN_EMAIL)~g;' \
+			-e 's~@MS_APP_ADMIN_PUBLIC_KEY@~$(shell cat "$(MS_APP_ADMIN_PUBLIC_KEY)" | tr "\n" "@  " | sed 's/@/\\n    /g')~g;' \
+			-e 's~@MS_APP_PUBLIC_KEY@~$(shell cat "$(MS_APP_PUBLIC_KEY)" | tr "\n" "@" | sed 's/@/\\n    /g')~g;' \
+			-e 's~@MS_APP_PRIVATE_KEY@~$(shell cat "$(MS_APP_PRIVATE_KEY)" | tr "\n" "@" | sed 's/@/\\n    /g')~g;' \
+		> "$@"
 
 clean_ms:
 	rm -f $(BUILD_MS)/ms/app.yaml
