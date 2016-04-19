1deb5fd8b7dae2d7a42d3d1bb6243caad647ce93
Zack Williams <zdw@artisancomputer.com>
2016-04-18 17:38:02 -0700

    yaml multiline fixes

--- ms/configure_ms.mk
+++ ms/configure_ms.mk
@@ -7,7 +7,7 @@ MS_FILES ?=
 BUILD_MS ?=
 
 # Where to put generated pub/priv keys
-KEY_FILES := $(MS_FILES)
+KEY_FILES ?= $(MS_FILES)
 
 MS_APP_NAME					?= syndicate-ms
 MS_APP_PUBLIC_HOST	?= localhost
@@ -37,13 +37,13 @@ $(BUILD_MS)/app.yaml: $(MS_FILES)/app.yamlin $(MS_APP_ADMIN_PUBLIC_KEY) $(MS_APP
 		sed -e 's~@MS_APP_NAME@~$(MS_APP_NAME)~g;' \
 				-e 's~@MS_APP_PUBLIC_HOST@~$(MS_APP_PUBLIC_HOST)~g;' \
 				-e 's~@MS_APP_ADMIN_EMAIL@~$(MS_APP_ADMIN_EMAIL)~g;' \
-				-e 's~@MS_APP_ADMIN_PUBLIC_KEY@~$(shell cat $(MS_APP_ADMIN_PUBLIC_KEY) | tr "\n" "@" | sed 's/@/\\n/g')~g;' \
-				-e 's~@MS_APP_PUBLIC_KEY@~$(shell cat $(MS_APP_PUBLIC_KEY) | tr "\n" "@" | sed 's/@/\\n/g')~g;' \
-				-e 's~@MS_APP_PRIVATE_KEY@~$(shell cat $(MS_APP_PRIVATE_KEY) | tr "\n" "@" | sed 's/@/\\n/g')~g;' \
+				-e 's~@MS_APP_ADMIN_PUBLIC_KEY@~$(shell cat $(MS_APP_ADMIN_PUBLIC_KEY) | tr "\n" "@  " | sed 's/@/\\n    /g')~g;' \
+				-e 's~@MS_APP_PUBLIC_KEY@~$(shell cat $(MS_APP_PUBLIC_KEY) | tr "\n" "@" | sed 's/@/\\n    /g')~g;' \
+				-e 's~@MS_APP_PRIVATE_KEY@~$(shell cat $(MS_APP_PRIVATE_KEY) | tr "\n" "@" | sed 's/@/\\n    /g')~g;' \
 				> "$@"
 
 clean_ms:
-	rm -f $(MS_CONFIG_DIR)/ms/app.yaml
+	rm -f $(BUILD_MS)/ms/app.yaml
 
 clean_certs: clean_ms
 	rm -f $(MS_APP_ADMIN_PUBLIC_KEY) $(MS_APP_ADMIN_PRIVATE_KEY) $(MS_APP_PUBLIC_KEY) $(MS_APP_PRIVATE_KEY)
