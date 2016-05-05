796a5b4ece09c48e418c926a888ab726e38771e5
Jude Nelson <judecn@gmail.com>
2016-05-05 15:59:00 -0400

    remove dead code

--- ms/appengine_config.py
+++ ms/appengine_config.py
@@ -1,5 +1,5 @@
 """
-   Copyright 2013 The Trustees of Princeton University
+   Copyright 2016 The Trustees of Princeton University
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -17,19 +17,3 @@
 appstats_MAX_STACK = 20
 appstats_CALC_RPC_COSTS = True
 
-# copied from https://github.com/dound/gae-sessions/blob/master/demo/appengine_config.py
-"""
-from openid.gaesession import SessionMiddleware, SESSION_COOKIE_KEY, delete_expired_sessions
-import os
-
-def webapp_add_wsgi_middleware(app):
-
-  #Does this get called too often?
-  #while delete_expired_sessions() is False:
-    #pass
-
-  from google.appengine.ext.appstats import recording
-  app = SessionMiddleware(app, cookie_key=SESSION_COOKIE_KEY)
-  app = recording.appstats_wsgi_middleware(app)
-  return app
-"""
