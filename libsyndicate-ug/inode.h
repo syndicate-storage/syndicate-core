3e79391c16c0edf1251fc258c4e4449925debfc4
Jude Nelson <judecn@gmail.com>
2016-09-15 00:05:43 -0400

    ref/set inode name

--- libsyndicate-ug/inode.h
+++ libsyndicate-ug/inode.h
@@ -131,6 +131,7 @@ int UG_inode_unlock( struct UG_inode* inode );
 uint64_t UG_inode_volume_id( struct UG_inode* inode );
 uint64_t UG_inode_coordinator_id( struct UG_inode* inode );
 char* UG_inode_name( struct UG_inode* inode );
+char* UG_inode_name_ref( struct UG_inode* inode );
 uint64_t UG_inode_file_id( struct UG_inode* inode );
 int64_t UG_inode_file_version( struct UG_inode* inode );
 int64_t UG_inode_write_nonce( struct UG_inode* inode );
@@ -161,6 +162,7 @@ bool UG_inode_is_dirty( struct UG_inode* inode );
 struct fskit_entry* UG_inode_resolve_path_and_parent( struct fskit_core* fs, char const* fs_path, bool writelock, int* rc, uint64_t* parent_id );
 
 // setters
+int UG_inode_set_name( struct UG_inode* inode, char const* name );
 void UG_inode_set_file_version( struct UG_inode* inode, int64_t version );
 void UG_inode_set_write_nonce( struct UG_inode* inode, int64_t wn );
 void UG_inode_set_xattr_nonce( struct UG_inode* inode, int64_t xn );
