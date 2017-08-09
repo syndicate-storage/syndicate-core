22a5427ba389b31323fa5217f73a702d3eb6fbbc
Jude Nelson <judecn@gmail.com>
2017-08-09 02:55:30 -0400

    have the ms record trace logs

--- ms/appengine_config.py
+++ ms/appengine_config.py
@@ -17,3 +17,7 @@
 appstats_MAX_STACK = 20
 appstats_CALC_RPC_COSTS = True
 
+def webapp_add_wsgi_middleware(app):
+    from google.appengine.ext.appstats import recording
+    app = recording.appstats_wsgi_middleware(app)
+    return app
