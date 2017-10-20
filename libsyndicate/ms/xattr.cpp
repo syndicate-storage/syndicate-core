fb9ea20ee4c33aabce191f983e7db4ef90032eb0
Jack Pogue <jpogue@cs.arizona.edu>
2017-10-20 10:41:06 -0700

    Clean up some docs formatting

--- libsyndicate/ms/xattr.cpp
+++ libsyndicate/ms/xattr.cpp
@@ -33,31 +33,30 @@
 /**
  * @brief Swap size bytes between a_ and b_
  *
- * Borrowed from https://github.com/stevengj/nlopt/blob/master/util/qsort_r.c
- * the algorithms swap() and nlopt_qsort_r() are subject to the following copyright notice:
-
-```
-  Copyright 2007-2014 MIT
-  Permission is hereby granted, free of charge, to any person obtaining
-  a copy of this software and associated documentation files (the
-  "Software"), to deal in the Software without restriction, including
-  without limitation the rights to use, copy, modify, merge, publish,
-  distribute, sublicense, and/or sell copies of the Software, and to
-  permit persons to whom the Software is furnished to do so, subject to
-  the following conditions:
-  
-  The above copyright notice and this permission notice shall be
-  included in all copies or substantial portions of the Software.
-  
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-```
-
+---
+ *
+ * Borrowed from https://github.com/stevengj/nlopt/blob/master/util/qsort_r.c\n
+ * the algorithms swap() and nlopt_qsort_r() are subject to the following copyright notice:\n
+ * \n
+ * Copyright 2007-2014 MIT\n
+ * Permission is hereby granted, free of charge, to any person obtaining\n
+ * a copy of this software and associated documentation files (the\n
+ * "Software"), to deal in the Software without restriction, including\n
+ * without limitation the rights to use, copy, modify, merge, publish,\n
+ * distribute, sublicense, and/or sell copies of the Software, and to\n
+ * permit persons to whom the Software is furnished to do so, subject to\n
+ * the following conditions:\n
+ * \n
+ * The above copyright notice and this permission notice shall be\n
+ * included in all copies or substantial portions of the Software.\n
+ * \n
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,\n
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF\n
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND\n
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE\n
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION\n
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION\n
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 static void swap(void *a_, void *b_, size_t size)
 {
